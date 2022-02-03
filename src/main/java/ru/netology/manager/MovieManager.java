800a9ad5cc2869ab61fd8fd5deb24cc10beb77cd
Leiza111 <leiza-rad@yandx.ru>
2022-02-03 09:47:00 +0300

    added MovieManagerTest.java, MovieManager.java, Movie.java

---
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -0,0 +1,50 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class MovieManager {
+    private Movie[] movies = new Movie[0];
+    private int recentMovies = 10;
+
+    //конструкторы
+    public MovieManager() { //конструктор без параметров
+    }
+
+    public MovieManager(int recentMovies) { //конст.с  параметром
+        this.recentMovies = recentMovies;
+    }
+
+    public void addMovie(Movie movie) {
+        // создаём новый массив размером на единицу больше
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        //копируем
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        // кладём последним наш элемент
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+//    //найди все элементы, которые ты заполнил
+//    public Movie[] () {
+//        return movies;
+//    }
+
+    //вывод последних фильмов
+    public Movie[] getRecentMovie() {
+        int resultLength;
+        if (recentMovies < movies.length) {
+            resultLength = recentMovies;
+        } else {
+            resultLength = movies.length;
+        }
+        Movie[] result = new Movie[resultLength];
+        for (int i=0; i < resultLength; i++){
+            int index = movies.length - i - 1;
+            result[i] = movies[index];
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/ru/netology/manager/MovieManagerTest.java
@@ -0,0 +1,113 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Movie;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MovieManagerTest {
+    private MovieManager manager = new MovieManager();
+    private  Movie first = new Movie(1, "First films", "drama");
+    private Movie second = new Movie(2, "Second films", "comedy");
+    private Movie third = new Movie(3, "Third films", "fantasy");
+    private Movie fourth = new Movie(4, "Fourth films", "drama");
+    private Movie fifth = new Movie(5, "Fifth films", "fantasy");
+    private Movie sixth = new Movie(6, "Sixth films", "cartoon");
+    private Movie seventh = new Movie(7, "Seventh films", "comedy");
+    private Movie eighth = new Movie(8, "Eightht films", "drama");
+    private Movie ninth = new Movie(9, "Ninth films", "fantasy");
+    private Movie tenth = new Movie(10, "Tenth films", "drama");
+
+    @Test
+    public void shouldGiveFilmsLimit(){
+        MovieManager man = new MovieManager(10);
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+        manager.addMovie(tenth);
+
+        Movie[] expected = new Movie[]{tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.getRecentMovie();
+        assertArrayEquals(expected, actual);
+            }
+
+    @Test
+    public void shouldGiveFilmsLessLimit(){
+        MovieManager man = new MovieManager(7);
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+
+        Movie[] expected = new Movie[]{seventh, sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.getRecentMovie();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveFilmsLimitBorders9(){
+        MovieManager man = new MovieManager(9);
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+
+
+        Movie[] expected = new Movie[]{ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.getRecentMovie();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveFilmsLimitBorders11(){
+        MovieManager man = new MovieManager(11);
+        manager.addMovie(first);
+        manager.addMovie(second);
+        manager.addMovie(third);
+        manager.addMovie(fourth);
+        manager.addMovie(fifth);
+        manager.addMovie(sixth);
+        manager.addMovie(seventh);
+        manager.addMovie(eighth);
+        manager.addMovie(ninth);
+        manager.addMovie(tenth);
+
+        Movie[] expected = new Movie[]{tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second, first};
+        Movie[] actual = manager.getRecentMovie();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveFilmsLimitBorders1(){
+        MovieManager man = new MovieManager(1);
+        manager.addMovie(first);
+
+        Movie[] expected = new Movie[]{first};
+        Movie[] actual = manager.getRecentMovie();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldGiveFilmsLimitBorders0(){
+        MovieManager man = new MovieManager(0);
+
+        Movie[] expected = new Movie[]{};
+        Movie[] actual = manager.getRecentMovie();
+        assertArrayEquals(expected, actual);
+    }
+
+}
